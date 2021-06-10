3696422ab8416747ed8e75e031c64a874d417a6e
Sharan J <50091666+shrn01@users.noreply.github.com>
2021-06-10 14:33:43 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 function getWeather() {
   let lat;
   let long;
-  // let today = Date();
-  // document.querySelector(".time").innerHTML = today.getTime();
-  // document.querySelector(".day").innerHTML = today.getDay();
-  // document.querySelector(".date").innerHTML = today.getDate();
+  let colors = {
+    "01d": "lightred",
+    "02d": "lightsalmon",
+    "03d": "whitesmoke",
+    "04d": "whitesmoke",
+    "09d": "cadetblue",
+    "10d": "cadetblue",
+    "11d": "skyblue",
+    "13d": "azure",
+    "50d": "azure",
+
+    "01n": "#9e6c83",
+    "02n": "#9e6c83",
+    "03n": "darkslategray",
+    "04n": "darkslategray",
+    "09n": "darkslategray",
+    "10n": "darkslategray",
+    "11n": "darkslategray",
+    "13n": "darkslategray",
+    "50n": "darkslategray"
+  };
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       lat = position.coords.latitude;
@@ -19,19 +36,32 @@ function getWeather() {
           const { main } = data.weather[0];
           const { temp } = data.main;
           const { humidity } = data.main;
-          const { pressure } = data.main;
           const { speed } = data.wind;
           document.querySelector(".temp").innerHTML =
             (temp - 273.15).toFixed(1) + "°C";
           document.querySelector(".weather").innerHTML = main;
           document.querySelector(".place").innerHTML = data.name;
-          document.querySelector(".humidity").innerHTML = humidity + "%";
-          document.querySelector(".pressure").innerHTML = pressure + "Pa";
-          document.querySelector(".windspeed").innerHTML = speed + "m/s";
+          document.querySelector(".humidity").innerHTML =
+            "<img src = 'humidity.png' class = 'humid-icon'/>" + humidity + "%";
+          document.querySelector(".windspeed").innerHTML =
+            "<img src = 'wind-speed.png' class = 'humid-icon'/>" +
+            speed +
+            "m/s";
           document.querySelector(".icon").src =
             "http://openweathermap.org/img/wn/" +
             data.weather[0].icon +
             "@2x.png";
+          document
+            .querySelector(":root")
+            .style.setProperty(
+              "--bg-color",
+              colors[String(data.weather[0].icon)]
+            );
+          if (data.weather[0].icon.includes("n")) {
+            document
+              .querySelector(":root")
+              .style.setProperty("--text-color", "white");
+          }
         });
     });
   }
